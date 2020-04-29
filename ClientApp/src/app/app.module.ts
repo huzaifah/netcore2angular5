bc141d0fbd1a68662b4186f4352161d19781a700
huzaifahdzulkifli <huzaifahd@hotmail.com>
2020-04-29 09:18:48 +0800

    Implemented Reactive form for quiz creation and edit

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
@@ -45,6 +45,7 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     FontAwesomeModule,
     HttpClientModule,
+    ReactiveFormsModule,
     FormsModule,
     RouterModule.forRoot([
       { path: '', redirectTo: 'home', pathMatch: 'full' },
--- ClientApp/src/app/components/quiz/quiz-edit.component.ts
+++ ClientApp/src/app/components/quiz/quiz-edit.component.ts
@@ -1,5 +1,6 @@
 import { Component, Inject, OnInit } from "@angular/core";
 import { ActivatedRoute, Router } from "@angular/router";
+import { FormGroup, FormControl, FormBuilder, Validators } from "@angular/forms";
 import { HttpClient } from "@angular/common/http";
 
 @Component({
@@ -11,14 +12,18 @@ import { HttpClient } from "@angular/common/http";
 export class QuizEditComponent {
   title: string;
   quiz: Quiz;
+  form: FormGroup;
 
   editMode: boolean;
 
   constructor(private activatedRoute: ActivatedRoute,
     private router: Router,
     private http: HttpClient,
+    private fb: FormBuilder,
     @Inject('BASE_URL') private baseUrl: string) {
+
     this.quiz = <Quiz>{};
+    this.createForm();
 
     var id = +this.activatedRoute.snapshot.params["id"];
     if (id) {
@@ -27,6 +32,7 @@ export class QuizEditComponent {
       this.http.get<Quiz>(url).subscribe(res => {
         this.quiz = res;
         this.title = "Edit - " + this.quiz.Title;
+        this.updateForm();
       }, error => console.error(error));
     }
     else {
@@ -35,12 +41,21 @@ export class QuizEditComponent {
     }
   }
 
-  onSubmit(quiz: Quiz) {
+  onSubmit() {
+
+    var tempQuiz = <Quiz>{};
+    tempQuiz.Title = this.form.value.Title;
+    tempQuiz.Description = this.form.value.Description;
+    tempQuiz.Text = this.form.value.Text;
+
     var url = this.baseUrl + "api/quiz";
 
     if (this.editMode) {
+
+      tempQuiz.Id = this.quiz.Id;
+
       this.http
-        .post<Quiz>(url, quiz)
+        .post<Quiz>(url, tempQuiz)
         .subscribe(res => {
           var v = res;
           console.log("Quiz " + v.Id + " has been updated.");
@@ -49,13 +64,48 @@ export class QuizEditComponent {
     }
     else {
       this.http
-        .put<Quiz>(url, quiz)
+        .put<Quiz>(url, tempQuiz)
         .subscribe(res => {
           var q = res;
           console.log("Quiz " + q.Id + " has been created.");
           this.router.navigate(["home"]);
         }, error => console.log(error));
     }
+  }
+
+  createForm() {
+    this.form = this.fb.group({
+      Title: ['', Validators.required],
+      Description: '',
+      Text: ''
+    })
+  }
+
+  updateForm() {
+    this.form.setValue({
+      Title: this.quiz.Title,
+      Description: this.quiz.Description || '',
+      Text: this.quiz.Text || ''
+    });
+  }
+
+  getFormControl(name: string) {
+    return this.form.get(name);
+  }
+
+  isValid(name: string) {
+    var e = this.getFormControl(name);
+    return e && e.valid;
+  }
+
+  isChanged(name: string) {
+    var e = this.getFormControl(name);
+    return e && (e.dirty || e.touched);
+  }
+
+  hasError(name: string) {
+    var e = this.getFormControl(name);
+    return e && (e.dirty || e.touched) && !e.valid;
   }
 
   onBack() {
