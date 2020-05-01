d36df8c9961e6da44c1eb12fb1370437f61781f8
huzaifahdzulkifli <huzaifahd@hotmail.com>
2020-05-01 13:36:22 +0800

    Implemented form validation for ResultEditComponent

--- ClientApp/src/app/components/result/result-edit.component.ts
+++ ClientApp/src/app/components/result/result-edit.component.ts
@@ -1,5 +1,6 @@
 import { Component, Inject, OnInit } from "@angular/core";
 import { ActivatedRoute, Router } from "@angular/router";
+import { FormGroup, FormControl, FormBuilder, Validators } from "@angular/forms";
 import { HttpClient } from "@angular/common/http";
 
 @Component({
@@ -11,16 +12,19 @@ import { HttpClient } from "@angular/common/http";
 export class ResultEditComponent {
   title: string;
   result: Result;
+  form: FormGroup;
 
   editMode: boolean;
 
   constructor(private activatedRoute: ActivatedRoute,
     private router: Router,
     private http: HttpClient,
+    private fb: FormBuilder,
     @Inject('BASE_URL') private baseUrl: string
   ) {
 
     this.result = <Result>{};
+    this.createForm();
 
     var id = +this.activatedRoute.snapshot.params["id"];
 
@@ -31,6 +35,8 @@ export class ResultEditComponent {
       this.http.get<Result>(url).subscribe(res => {
         this.result = res;
         this.title = "Edit - " + this.result.Text;
+        this.updateForm();
+
       }, error => console.error(error));
     } else {
       this.result.QuizId = id;
@@ -38,24 +44,66 @@ export class ResultEditComponent {
     }
   }
 
-  onSubmit(result: Result) {
+  onSubmit() {
     var url = this.baseUrl + "api/result";
 
+    var tempResult = <Result>{};
+    tempResult.Text = this.form.value.Text;
+    tempResult.MinValue = this.form.value.MinValue;
+    tempResult.MaxValue = this.form.value.MaxValue;
+    tempResult.QuizId = this.result.QuizId;
+
     if (this.editMode) {
-      this.http.post<Result>(url, result)
+      tempResult.Id = this.result.Id;
+      this.http.post<Result>(url, tempResult)
         .subscribe(res => {
           var v = res;
           console.log("Result " + v.Id + " has been updated.");
           this.router.navigate(["quiz/edit", v.QuizId]);
         }, error => console.log(error));
     } else {
-      this.http.put<Result>(url, result)
+      this.http.put<Result>(url, tempResult)
         .subscribe(res => {
           var v = res;
           console.log("Result " + v.Id + " has been created.");
           this.router.navigate(["quiz/edit", v.QuizId]);
         }, error => console.log(error));
     }
+  }
+
+  createForm() {
+    this.form = this.fb.group({
+      Text: ['', Validators.required],
+      MinValue: ['', Validators.pattern(/^\d*$/)],
+      MaxValue: ['', Validators.pattern(/^\d*$/)]
+    })
+  }
+
+  updateForm() {
+    this.form.setValue({
+      Text: this.result.Text,
+      MinValue: this.result.MinValue || '',
+      MaxValue: this.result.MaxValue || ''
+    });
+  }
+
+  getFormControl(name: string) {
+    return this.form.get(name);
+  }
+
+  isValid(name: string) {
+    var e = this.getFormControl(name);
+    return e && e.valid;
+  }
+
+  isChanged(name: string) {
+    var e = this.getFormControl(name);
+    return e && (e.dirty || e.touched);
+  }
+
+  hasError(name: string) {
+    var e = this.getFormControl(name);
+    return e && (e.dirty || e.touched) && !e.valid;
   }
 
   onBack() {
